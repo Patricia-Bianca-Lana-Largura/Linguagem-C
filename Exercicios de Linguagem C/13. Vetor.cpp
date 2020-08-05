a4376562dcd99d293681564ad0c7fb0bb63b0d7e
Patricia Bianca Lana Largura <patricia.largura@outlook.com>
2020-08-05 20:47:05 -0300

    Create 13. Vetor.cpp

---
+++ Exercicios de Linguagem C/13. Vetor.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	
+	int vetorA[30];
+	int i;
+	
+	for (i=0; i<30; i++)
+	{
+		printf("Informe o %d elemento: ",i);
+		scanf("%d",&vetorA[i]);
+	}
+	
+	for (i=29; i>=0; i--)
+	{
+		printf("\n %d", vetorA[i]);
+	}
+	return(0);
+}
